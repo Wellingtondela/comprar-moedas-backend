22177b9aed03629a84fa840dbb9143c4f7188613
Wellingtondela <wsatsg@gmail.com>
2025-06-07 02:43:56 -0300

    hdghdgdg

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,16 @@
-const admin = require('firebase-admin');
-const path = require('path');
+const admin = require("firebase-admin");
+require("dotenv").config();
 
-const serviceAccount = require(path.resolve(__dirname, 'serviceAccountKey.json'));
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  token_uri: "https://oauth2.googleapis.com/token",
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
